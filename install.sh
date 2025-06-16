ac8a7f1cffaea75bf45c1bbe2b6e62ac4c391aa9
KaiKimera <git@kai.kim>
2025-06-16 15:22:19 +0300

    Update resources

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ function installing() {
   for i in "${f[@]}"; do
     [[ -f "${i}" ]] || continue
     install -m '0644' -Dt "${DIR}" "${i}"
-    [[ "${i}" == *.sh ]] && chmod +x "${DIR}/${i}"
+    chmod +x "${DIR}/${i}.sh"
     [[ "${i}" == cron.* ]] && ln -sf "${DIR}/${i}" "/etc/cron.d/${i//./_}"
   done
 }
